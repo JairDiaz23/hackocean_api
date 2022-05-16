c64f9b6c3374f475f813f580a10171fd85b57f56
Jair diaz <mjdiaz@ccg.unam.mx>
2022-05-15 19:20:16 -0500

    Prototype finished. Cors enable and two web services finished

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 // Load express
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 // Load body parser
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors())
 
 // Mongo loading file
 const mongoose = require('./config/db');
